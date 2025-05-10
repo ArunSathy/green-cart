d73f0a7f70f1d9bf2525ad5f4641bfc2f7091c38
Arun Sathyan <arunsathyan660@gmail.com>
2025-05-10 15:12:34 +0530

    Deployment completed by adding frontend live URLs

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -51,7 +51,6 @@ const Navbar = () => {
             <div className="hidden sm:flex items-center gap-8">
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/products'>All Products</NavLink>
-                <NavLink to='/contact'>Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
@@ -102,7 +101,6 @@ const Navbar = () => {
                     {user &&
                         <NavLink to='/' onClick={() => setOpen(false)}>My Orders</NavLink>
                     }
-                    <NavLink to='/' onClick={() => setOpen(false)}>Contact</NavLink>
 
                     {!user ? (
                         <button onClick={() => {
