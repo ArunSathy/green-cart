d5e34151ecfb8b0080efd2651697cd8964a5c0a6
Arun Sathyan <arunsathyan660@gmail.com>
2025-05-10 18:03:54 +0530

    Deployment update for the logout function for the user

--- server/server.js
+++ server/server.js
@@ -26,7 +26,10 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebHook)
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}))
+app.use(cors({origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
+    callback(new Error('Not allowed by CORS'));
+  }, credentials: true}))
 
 app.get('/', (req,res) => res.send('API is Working'));
 app.use('/api/user', userRouter);
